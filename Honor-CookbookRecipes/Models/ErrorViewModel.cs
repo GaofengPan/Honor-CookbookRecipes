8c51edb139f638caaa60a3a1d3abb546e6291eb6
GaofengPan <1254066225@qq.com>
2019-05-08 15:05:27 +0800

    添加项目文件。

---
+++ Honor-CookbookRecipes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Honor_CookbookRecipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
